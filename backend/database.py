107337d28e98dbd5a9bb1c27c7440d765a945237
Sophia Nguyen <sophia.nguyen@wisetechglobal.com>
2026-03-29 02:27:44 +1100

    set up dmongo db

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,12 @@
+import os
 import motor.motor_asyncio
+from dotenv import load_dotenv
 
-# TODO: configure connection
-client = None
-db = None
-flashcards_collection = None
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "flashcards_db")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+db = client[DB_NAME]
+flashcards_collection = db["flashcards"]
